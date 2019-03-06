1e9ee30cc139fbcb696a0402ccbfa11fda17af0b
Maciej Kurek <maciejk@mac-maciejk.int.giffgaff.co.uk>
2019-03-06 14:02:56 +0000

    #9 inclusive, basic BookType schema

--- server/app.js
+++ server/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema  //schema: schema
+}));
+
 
 app.listen(4000, () => {
     console.log('Listening on port 4000...')
---
+++ server/schema/schema.js
@@ -0,0 +1,31 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const BookType = new GraphQLObjectType({
+   name: 'Book',
+   fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString }
+   }) 
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {id: { type: GraphQLString }},
+            resolve(parent, args) {
+                // code to get data from DB or other source
+                return _.find(books, { id: args.id })
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
